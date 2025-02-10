589fe5935b9c845651b29c28ab84692d599f84ba
Bartosz <meatinaplasticsack@gmail.com>
2025-02-10 11:24:03 +0100

    Small fixes to the venvs.

--- run.bat
+++ run.bat
@@ -2,12 +2,12 @@
 call .venv\Scripts\activate.bat
 
 :: Running only tests marked as smoke. Add or remove "rem" at the beggining of the line.
-pytest -m -s -v "smoke" --html .\reports\report.html
+pytest -s -v -m "smoke" --html .\reports\report.html
 
 :: Running only tests marked as regression. Add or remove "rem" at the beggining of the line.
-rem pytest -m -s -v "regression" --html .\reports\report.html
+rem pytest -s -v -m "regression" --html .\reports\report.html
 
 :: Running all tests. Add or remove "rem" at the beggining of the line.
-rem pytest -m -s -v "smoke and regression" --html .\reports\report.html
+rem pytest -s -v -m "smoke and regression" --html .\reports\report.html
 
 pause
